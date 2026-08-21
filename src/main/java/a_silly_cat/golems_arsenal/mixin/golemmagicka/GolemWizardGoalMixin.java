ff0e5f656d670b464ff2c96c35c5a99381626a59
Asillycat-glitch <Asillycat-glitch@users.noreply.github.com>
2026-08-21 14:20:10 +0800

    Diagnostics: log spell cooldown state and mana cost in final spell pool

--- src/main/java/a_silly_cat/golems_arsenal/mixin/golemmagicka/GolemWizardGoalMixin.java
+++ src/main/java/a_silly_cat/golems_arsenal/mixin/golemmagicka/GolemWizardGoalMixin.java
@@ -1,11 +1,17 @@
 package a_silly_cat.golems_arsenal.mixin.golemmagicka;
 
+import a_silly_cat.golems_arsenal.compat.golemmagicka.GolemScrollData;
 import a_silly_cat.golems_arsenal.compat.golemmagicka.GolemScrollModifier;
 import com.mojang.logging.LogUtils;
 import dev.xkmc.golemmagicka.content.entity.GolemMagicData;
 import dev.xkmc.golemmagicka.content.entity.GolemWizardGoal;
 import dev.xkmc.golemmagicka.content.entity.SpellEntry;
 import dev.xkmc.modulargolems.content.entity.common.AbstractGolemEntity;
+import io.redspace.ironsspellbooks.api.magic.MagicData;
+import io.redspace.ironsspellbooks.api.registry.AttributeRegistry;
+import io.redspace.ironsspellbooks.api.registry.SpellRegistry;
+import io.redspace.ironsspellbooks.api.spells.AbstractSpell;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.util.random.SimpleWeightedRandomList;
 import org.slf4j.Logger;
 import org.spongepowered.asm.mixin.Mixin;
@@ -34,7 +40,14 @@ public abstract class GolemWizardGoalMixin {
         if (golem.tickCount % 40 != 0) {
             return;
         }
-        LOGGER.info("[GolemsArsenal] scroll golem {} final available spell pool empty={}, mana={}",
-                golem, cir.getReturnValue().isEmpty(), data.getMagicData().getMana());
+        MagicData md = data.getMagicData();
+        ResourceLocation spellId = GolemScrollData.getSpellId(golem);
+        int level = GolemScrollData.getLevel(golem);
+        AbstractSpell spell = spellId == null ? null : SpellRegistry.getSpell(spellId);
+        boolean cooldown = spell != null && md.getPlayerCooldowns().isOnCooldown(spell);
+        LOGGER.info("[GolemsArsenal] scroll golem {} final pool empty={}, mana={}/{}, spell={} lv{}, cost={}, cooldown={}",
+                golem, cir.getReturnValue().isEmpty(), md.getMana(),
+                (int) golem.getAttributeValue(AttributeRegistry.MAX_MANA.get()),
+                spellId, level, spell == null ? -1 : spell.getManaCost(level), cooldown);
     }
 }
